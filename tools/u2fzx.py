4290e1568b331356ef69a517599e7a3cec6ccdc3
Michael Hope <mlhx@google.com>
2018-03-18 17:43:58 +0100

    tools: added a tool to erase and upload the attestation keys.
    Signed-off-by: Michael Hope <mlhx@google.com>

---
+++ tools/u2fzx.py
@@ -0,0 +1,23 @@
+import ecdsa
+
+from u2flib_host import u2f
+from u2flib_host import constants
+
+U2F_VENDOR_JUJU = 0xc9
+INS_ERASE = 0xC1
+INS_SET_CERTIFICATE = 0xC2
+INS_SET_PRIVATE_KEY = 0xC3
+
+def main():
+    with u2f.list_devices()[0] as dev:
+        dev.send_apdu(U2F_VENDOR_JUJU, INS_ERASE)
+
+        with open('key.pem') as f:
+            key = ecdsa.SigningKey.from_pem(f.read())
+            dev.send_apdu(U2F_VENDOR_JUJU, INS_SET_PRIVATE_KEY, data=key.to_string())
+
+        with open('cert.der', 'rb') as f:
+            dev.send_apdu(U2F_VENDOR_JUJU, INS_SET_CERTIFICATE, data=f.read())
+
+
+main()
